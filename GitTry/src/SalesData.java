02ea53f8b5dd059f46e2412c65e11e2021f132e3
Rsamal <rsamal@us.ibm.com>
2016-02-14 20:25:48 -0800

    Added display data

--- GitTry/src/SalesData.java
+++ GitTry/src/SalesData.java
@@ -3,8 +3,9 @@ public class SalesData {
 	int data[] ={ 10, 20 ,30, 40};
 	
 	void display(){
+		System.out.println("Displaying data ....");
 		for(int i=0;i < data.length;i++){
-			System.out.println("");
+			System.out.println("nextvalue:"+data[i]);
 		}
 	}
 }
