fb910744ab62cc487a72592826f8b8a423bd4b8a
Rsamal <rsamal@us.ibm.com>
2016-02-14 20:19:51 -0800

    Partial implementation of displaying the numbers

--- GitTry/src/SalesData.java
+++ GitTry/src/SalesData.java
@@ -3,6 +3,8 @@ public class SalesData {
 	int data[] ={ 10, 20 ,30, 40};
 	
 	void display(){
-		System.out.println("TODO: Implement sales display data");
+		for(int i=0;i < data.length;i++){
+			System.out.println("");
+		}
 	}
 }
