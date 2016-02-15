b17c161dea5b1dd5848e69ed37c6bc5c30a9d0d7
Rsamal <rsamal@us.ibm.com>
2016-02-14 19:59:42 -0800

    initial checkin of sales tool

---
+++ GitTry/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	public static void displayGreeting(){
+		System.out.println("Hello Happy Sles Team");
+		System.out.println("This app shows Sales Data");
+	}
+	
+}
---
+++ GitTry/src/SalesData.java
@@ -0,0 +1,8 @@
+
+public class SalesData {
+	int data[] ={ 10, 20 ,30, 40};
+	
+	void display(){
+		System.out.println("TODO: Implement sales display data");
+	}
+}
