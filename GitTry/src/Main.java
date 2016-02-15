7004314af1a4e29e3656ab44d6cb13127c6367d1
Rsamal <rsamal@us.ibm.com>
2016-02-14 20:04:52 -0800

    Adding a new line

--- GitTry/src/Main.java
+++ GitTry/src/Main.java
@@ -9,6 +9,8 @@ public class Main {
 	public static void displayGreeting(){
 		System.out.println("Hello Happy Sles Team");
 		System.out.println("This app shows Sales Data");
+		
+		System.out.println("You have changed her");
 	}
 	
 }
